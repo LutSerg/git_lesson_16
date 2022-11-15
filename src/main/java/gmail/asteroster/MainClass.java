3f9528795b600b5aabc2398e6109afd3c6152e76
LutSerg <5QGJAZCqesdHUvk>
2022-11-15 21:40:48 +0300

    First commit

---
+++ src/main/java/gmail/asteroster/MainClass.java
@@ -0,0 +1,8 @@
+package gmail.asteroster;
+
+public class MainClass {
+    public static void main(String[] args) {
+        System.out.println("Hello, everybody!");
+        System.out.println("some new text");
+    }
+}
